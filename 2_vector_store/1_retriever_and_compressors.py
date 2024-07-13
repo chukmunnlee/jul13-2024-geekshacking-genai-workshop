2e1876e77621d92ab59c01d58b27cbcb8115718c
chukmunnlee <chukmunnlee@gmail.com>
2024-07-13 17:16:13 +0800

    vector db

--- 2_vector_store/1_retriever_and_compressors.py
+++ 2_vector_store/1_retriever_and_compressors.py
@@ -12,3 +12,33 @@ db_dir = "./chroma_db"
 
 vector_store = load()
 
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.1)
+
+# Create custom compressor prompt
+prompt = PromptTemplate.from_template("""
+   Given the following question and context, summarise the context
+   that is relevant to answer the question
+
+   Question: {question}
+   Context: 
+   >>>
+      {context}
+   >>>
+""")
+
+# create context compressor
+compressor = LLMChainExtractor.from_llm(llm=llm, prompt=prompt)
+
+# compressor retriever
+retriever = ContextualCompressionRetriever(
+   base_retriever=vector_store.as_retriever(),
+   base_compressor=compressor
+)
+
+with get_openai_callback() as cb:
+   results = retriever.invoke(
+      input='what was the last wish', k=3)
+
+   for i, r in enumerate(results):
+      print(r.page_content, r.metadata['source'])
+      print()
--- 2_vector_store/2_llm_with_retrievers.py
+++ 2_vector_store/2_llm_with_retrievers.py
@@ -10,8 +10,47 @@ from langchain_community.callbacks import get_openai_callback
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
 
-
 db_dir = "./chroma_db"
 
 vector_store = load(db_dir)
 
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.1, verbose=True)
+
+# create context compressor
+compressor = LLMChainExtractor.from_llm(llm=llm)
+
+# compressor retriever
+document_retriever = ContextualCompressionRetriever(
+   base_retriever=vector_store.as_retriever(),
+   base_compressor=compressor
+)
+
+prompt = ChatPromptTemplate.from_messages([
+   ('system', """
+      Use the following piece of retrieved context to answer the question. 
+      If you do not know the answer, say I don't know.
+
+      {context}
+   """),
+   ('human', '{input}')
+])
+
+# create a stuff document chain
+stuff_document_chain  = create_stuff_documents_chain(
+   llm=llm, prompt=prompt
+)
+
+# retrieve doc -> stuff doc 
+retrieval_chain = create_retrieval_chain(document_retriever, stuff_document_chain)
+
+while True:
+   with get_openai_callback() as cb:
+      question = input('Question ')
+      if not bool(question):
+         break
+      result = retrieval_chain.invoke({ 'input': question })
+
+      print(f'Input: {result["input"]}')
+      print(f'Answer: {result["answer"]}')
+      print('\ncb: ', cb)
+      print('\n----------------------\n')
